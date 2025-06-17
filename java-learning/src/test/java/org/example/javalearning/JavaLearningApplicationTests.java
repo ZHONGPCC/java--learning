69ec8f954fca86af9406bb208a970d761cab9618
ZHONGPCC <33050564+ZHONGPCC@users.noreply.github.com>
2025-06-17 11:05:57 +0800

    Add files via upload
    初始化代码，test测试spring启动

---
+++ java-learning/src/test/java/org/example/javalearning/JavaLearningApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.javalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        System.out.println("Hello World");
+    }
+
+}
